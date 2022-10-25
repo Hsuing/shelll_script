d8b0281da584907f3b42063b418efd66c7ea282f
unknown <>
2022-10-25 15:19:00 +0800

    l

--- monitor/dingding/upstream_timeout.sh
+++ monitor/dingding/upstream_timeout.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #https://ding-doc.dingtalk.com/doc#/serverapi2/potcn9
-#########################################################################
+#auther: hanxiong
 #
 BIND_IP=`/sbin/ifconfig ${ETH0}| grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -1`
 # set variables
-TOKEN="https://oapi.dingtalk.com/robot/send?access_token=xxxxx"
+TOKEN="https://oapi.dingtalk.com/robot/send?access_token=xxx"
 cluster="$(hostname)"
-name="errorLog"
+name="uptimeout"
+
 function SendMsgToDingding() {
 TIME=`date +%F_%H:%M`
     curl $TOKEN -H 'Content-Type: application/json' -d "
@@ -21,8 +22,6 @@ TIME=`date +%F_%H:%M`
     }"
 }
 
-
-
 LOGERRORCOUNT=2
 CURRENTLOGLINES=0
 ERRORLINES=0
@@ -30,18 +29,17 @@ TYPE=$1
 FLAG=1
 LOGERROR='upstream timed out'
 LOGERROR2='no live upstreams while connecting to upstream'
-LOGERROR3='crit'
-#LOGERROR3='Connection_refused'
+LOGERROR3='Connection_refused'
 #0 for error
 Company=`hostname`
 
-Msg="`tail -1 /data/apps/apisix2.15/logs |sed 's/"/x/g' |sed '/ \+/s//_/g'|sed 's/%/percent/g'`,$Company"
+	Msg="`tail -1 /var/log/nginx/error.log|sed 's/"/x/g' |sed '/ \+/s//_/g'|sed 's/%/percent/g'`,$Company"
 
 function checkErrorLog(){
     DATES=`date +%Y%m%d`
     COUNTLOGNAME=count.log
     ERRORLOGNAME=error.log
-    MONIDIR=/opt/monitor/logs
+    MONIDIR=/opt/wslog
     LOGDIR=/var/log/nginx
 
     if [[ -d ${MONIDIR} ]];then
